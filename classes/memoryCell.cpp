90caaa50e62d2a4a862cae4ce9bb30f9547a9c57
John <gohammdata@gmail.com>
2023-11-13 08:21:35 -0500

    memoryCell class

---
+++ classes/memoryCell.cpp
@@ -0,0 +1,18 @@
+/**
+ * Like intCell but for any type
+*/
+template <typename Object>
+class MemoryCell
+{
+    public:
+        explicit MemoryCell( const Object & initialValue = Object() ) : storedValue( initialValue ){
+        }
+        const Object & read() const{
+            return storedValue;
+        }
+        void write( const Object & x ){
+            storedValue = x;
+        }
+    private:
+        Object storedValue;
+};
