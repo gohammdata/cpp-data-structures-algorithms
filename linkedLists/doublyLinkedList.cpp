ad4928fea80132a48f74c2fa305f3868f6a0b492
John <gohammdata@gmail.com>
2023-11-13 18:06:09 -0500

    doubly Linked List class. Node struct and current node pointer

---
+++ linkedLists/doublyLinkedList.cpp
@@ -0,0 +1,54 @@
+template <typename Object>
+class List {
+    private:
+        // Node data structure with object data, and pointers to a previous and next node (doubly linked).
+        struct Node {
+            Object data;
+            Node *prev;
+            Node *next;
+        //  Constructor for Node
+            Node( const Object & d = Object(), Node *p = NULL, Node *n = NULL ) : data( d ), prev ( p ), next( n ) {
+
+            }
+        }
+    public:
+        class const_iterator {
+            //  Stores a pointer to "current" node and provides implementation of iterator operations
+            public:
+                const_iterator() : current ( NULL ) {
+
+                }
+                const Object & operator* () const {
+                    return retrieve();
+                }
+                const_iterator & operator++ () {
+                    current = current->next;
+                    return *this;
+                }
+                const_iterator operator++ ( int ) {
+                    const_iterator old = *this;
+                    ++( *this );
+                    return old;
+                }
+
+                bool operator== ( const const_iterator & rhs ) const {
+                    return current == rhs.current;
+                }
+                bool operator!= ( const const_iterator & rhs ) const {
+                    return !( *this == rhs );
+                }
+            
+            protected:
+                Node *current;
+
+                Object & retrieve() const {
+                    return current->data;
+                }
+
+                const_iterator( Node *p ) : current( p ) {
+
+                }
+
+                friend class List<Object>;
+        }
+};
