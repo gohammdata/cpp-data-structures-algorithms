912fcafdde7c64589af35e59102b7066fe2a8cc9
John <gohammdata@gmail.com>
2023-11-26 15:20:09 -0500

    iterator class

--- linkedLists/doublyLinkedList.cpp
+++ linkedLists/doublyLinkedList.cpp
@@ -54,7 +54,41 @@ class List {
                 const_iterator( Node *p ) : current( p ) {
 
                 }
-
+                /**
+                 * Access private members of Nodes
+                */
                 friend class List<Object>;
+        }
+
+        class iterator : public const_iterator {
+            public:
+                iterator() {
+
+                }
+
+                Object & operator*() {
+                    return retrieve();
+                }
+                const Object & operator* () const {
+                    return const_iterator::operator*();
+                }
+
+                iteraotr & operator++ () {
+                    current = current->next;
+                    return *this;
+                }
+
+                iterator operator++ ( int ) {
+                    iterator old = *this;
+                    **( *this );
+                    return old;
+                }
+
+            protected:
+                iterator( Node *p ) : const_iterator( p ){
+
+                }
+
+                friend class List<Object>
         }
 };
